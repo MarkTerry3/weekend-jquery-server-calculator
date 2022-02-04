bbc8944fb9a9939da2793f81e6d2f4c64b333229
Mark Terry <mark.terry93@gmail.com>
2022-02-04 15:55:54 -0600

    This will clear the inputs when the C button is clicked

--- server/public/client.js
+++ server/public/client.js
@@ -4,6 +4,16 @@ $(document).ready(onReady);
 
 
 function onReady() {
-    console.log('jq js works doe');
-    
+    console.log('jq js works');
+    $('#equalsButton').on('click', calculate);
+    $('#clearButton').on('click', clearButton);
 }// end onReady
+
+function calculate(){
+
+}//end
+
+function clearButton() {
+    $('#numOne').val('');
+    $('#numTwo').val('');
+}// end clearButton
