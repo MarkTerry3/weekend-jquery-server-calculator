9840c3586e130e084add656f1de206167a6e5621
Mark Terry <mark.terry93@gmail.com>
2022-02-05 15:44:36 -0600

    almost there, cant append to DOM yet

--- server/public/client.js
+++ server/public/client.js
@@ -5,13 +5,87 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jq js works');
-    $('#equalsButton').on('click', calculate);
+    $('#equalsButton').on('click', postNumbers);
     $('#clearButton').on('click', clearButton);
+    $('#plusButton').on('click', operatorButtonClick);
+    $('#minusButton').on('click', operatorButtonClick);
+    $('#timesButton').on('click', operatorButtonClick);
+    $('#divideButton').on('click', operatorButtonClick);
 }// end onReady
 
-function calculate(){
+let choosenOperator;
 
-}//end
+function postNumbers() {
+            //get quotes from the server using AJAX
+            $.ajax({
+                method: 'POST', //STANDARD TO CAPITALIZE
+                url: '/numbers',
+                data: {
+                    numberInputs: {
+                        numOne: $('#numOne').val(),
+                        operator: choosenOperator,
+                        numTwo: $('#numTwo').val(),
+                        answer: ''
+                    }
+                }
+            }).then(function(response) {
+                // .then() is asynchronous, thats why we wait for the GET and put .then after
+                //standard practice to put response in the parameters
+                console.log('great success, in postNumbers', response);
+                //to do , append quotes to dom
+                // renderToDom(response);
+            }) .catch(function(response) {
+                //if the server sends an error, what will we do with that situation? - 
+                console.log('ope no luck, in postNumbers', response);
+            }) 
+}// end getNumbers
+
+function getNumbers() {
+            //get quotes from the server using AJAX
+            $.ajax({
+                method: 'GET', //STANDARD TO CAPITALIZE
+                url: '/numbersAnswer',
+            }).then(function(response) {
+                // .then() is asynchronous, thats why we wait for the GET and put .then after
+                //standard practice to put response in the parameters
+                console.log('great success, in numbersAnswer', response);
+                //to do , append quotes to dom
+                renderToDom(response);
+            }) .catch(function(response) {
+                //if the server sends an error, what will we do with that situation? - 
+                console.log('ope no luck, in numbersAnswer', response);
+            }) 
+}// end getNumbers
+
+
+
+
+
+function operatorButtonClick(params) {
+    console.log('in operatorButtonClick');
+// sets choosenOperastor to eqaul the name of the id of whatever they clicked on
+    choosenOperator = $(this).attr("id")
+    console.log($(this).attr("id"));
+    
+    
+}// end operatorButtonClick
+
+
+function renderToDom(inputsArray) {
+    for (let i = 0; i < inputsArray.length; i++) {
+        $('#answerOutput').append(`<li>${inputsArray.numOne} ${inputsArray.operator} ${inputsArray.numTwo} ${inputsArray.answer}</li>`)
+        
+    }
+    // append to a ul
+    
+}// end renderToDom
+
+
+
+
+
+
+
 
 function clearButton() {
     $('#numOne').val('');
--- server/server.js
+++ server/server.js
@@ -7,17 +7,59 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
 
+
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+let inputsArray = [];
 
 // express static file serving
 // folder name is server/public
 app.use(express.static('server/public'));
 
+app.post('/numbers', function (req, res) {
+    console.log('request at /numbers was made', req);
+    let newEquation = req.body.numberInputs;
+
+    x = req.body.numberInputs.numOne;
+    y = req.body.numberInputs.numTwo;
+    z = req.body.numberInputs.operator;
+    let answer = compute(x, y, z);
+
+    req.body.numberInputs['answer'] = answer;
+
+    inputsArray.push(newEquation);
+    console.log(newEquation);
+    res.send(200);
+ });
+
+let answer = 0;
+
+function compute(x, y, z) {
+    if (z === 'plusButton') {
+        answer = (x + y);
+        return answer;
+    }else if (z === 'minusButton') {
+        answer = (x - y);
+        return answer;
+    }else if (z === 'timesButton') {
+        answer = (x * y);
+        return answer;
+    }else if (z === 'divideButton') {
+        answer = (x / y);
+        return answer;
+    }
+    else {
+        console.log('Somethings not working');
+        
+    }
+}// end compute
 
 
-
-
-
+app.get('/numbersAnswer', function (req, res) {
+    console.log('request at /numbersAnswer was made', req);
+     res.send(inputsArray);
+ });
 
 
 
